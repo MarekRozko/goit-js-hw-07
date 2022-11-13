56978a12bb38120aba7597cb2ef4343deb1c97ed
Marek Rożko <maknamar12@gmail.com>
2022-11-13 14:22:26 +0100

    add task 1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const items = createImagesContent(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', items)
+
+
+function createImagesContent() {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+    })
+        .join('');
+}
+galleryContainer.addEventListener('click', openMadalImages)
+
+function openMadalImages(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    };
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+        {
+        onShow: () => window.addEventListener('keydown', modalClose),
+        onClose: () => window.removeEventListener('keydown', modalClose),
+        })
+    instance.show();
+
+function modalClose(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+        console.log(event)
+    }
+}};
